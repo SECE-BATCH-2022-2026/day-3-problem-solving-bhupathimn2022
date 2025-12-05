993137f471d4566de21c5d2a98c59437ad50c1ac
bhupathimn2022 <125240993+bhupathimn2022@users.noreply.github.com>
2025-12-05 11:45:36 +0530

    Replaced Hello.java with PG1.java

---
+++ PG1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class PG1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+        String t = sc.nextLine();
+
+        int count = 0;
+
+        for (int i = 0; i <= s.length() - t.length(); i++) {
+            if (s.substring(i, i + t.length()).equals(t)) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
